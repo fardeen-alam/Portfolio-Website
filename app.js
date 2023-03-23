6ce5ae4f7ececd5a2a8af4a3fcbe8126244a971b
Fardeen Alam <fardeen.alam@globallogic.com>
2023-03-23 12:49:45 +0530

    Template complete

--- app.js
+++ app.js
@@ -20,4 +20,32 @@ links.forEach((link) => {
             ease: "elastic.out(1, 0.5)",
         });
     });
+
+    const cards = document.querySelectorAll(".card");
+
+    cards.forEach((card, index) => {
+        card.addEventListener("click", () => {
+            //Get state for the flip
+            const state = Flip.getState(cards);
+
+            //Add active class to clicked one and add inactive to the others
+            const isCardActive = card.classList.contains("active");
+
+            cards.forEach((otherCard, otherIndex) => {
+                otherCard.classList.remove("active");
+                otherCard.classList.remove("is-inactive");
+                if (!isCardActive && index !== otherIndex) {
+                    otherCard.classList.add("is-inactive");
+                }
+            });
+
+            if (!isCardActive) card.classList.add("active");
+
+            Flip.from(state, {
+                duration: 0.7,
+                ease: 'Expo.out',
+                absolute: true,
+            })
+        });
+    });
 });
